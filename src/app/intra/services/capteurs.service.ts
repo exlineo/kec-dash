a0ae1e533e32f2d12480c0a41b1c55c42f8a8021
exlineo <contact@exlineo.com>
2025-07-23 10:08:48 +0200

    Début des requêtes sur les données

--- src/app/intra/services/capteurs.service.ts
+++ src/app/intra/services/capteurs.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Firestore, collection, QuerySnapshot, query, doc, getDocs, getDoc, setDoc, where } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CapteursService {
 
+  fire: Firestore = inject(Firestore);
+
+  kits: Array<any> = [];
+  capteurs: Array<any> = [];
+
   constructor() { }
+
+  getKits() {
+
+  }
+  getCapteurs() {
+
+  }
+  getCapteursByKit() {
+
+  }
+  async getCapteurByTemps(debut: number = 0, fin: number=Date.now()) {
+    // ref.where("timestamp", ">=", "2017-11").where("timestamp", "<", "2017-12")
+    const q = query(collection(this.fire, "kec-capteurs"), where("timestamp", ">=", debut), where("timestamp", "<", fin));
+    
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+  }
+  /** Créer un nouveau kit */
+  async setKit(kit: any) {
+    const ref = collection(this.fire, "kec-kits");
+    await setDoc(doc(ref, kit.id), kit);
+  }
 }
